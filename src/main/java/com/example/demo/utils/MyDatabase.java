50b60025faa5db90d7bd98ac6fc6151e9da686c0
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2025-02-28 19:58:25 +0100

    fixin my database

--- src/main/java/com/example/demo/utils/MyDatabase.java
+++ src/main/java/com/example/demo/utils/MyDatabase.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class MyDatabase {
     private static MyDatabase instance;
     private final String URL ="jdbc:mysql://127.0.0.1:3306/cov";
