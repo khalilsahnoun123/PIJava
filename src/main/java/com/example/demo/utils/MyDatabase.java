be4d319d210e3ef230b617e61287b1e9ceded2d9
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2025-03-01 10:46:16 +0100

    adding mydatabase class

--- src/main/java/com/example/demo/utils/MyDatabase.java
+++ src/main/java/com/example/demo/utils/MyDatabase.java
@@ -8,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
+
 public class MyDatabase {
     private static MyDatabase instance;
     private final String URL ="jdbc:mysql://127.0.0.1:3306/cov";
